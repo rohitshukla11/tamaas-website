0aea21a26aa4a6c3a41fb5fe7d8acd734f676c78
sindhwani.himanshu <sindhwani.himanshu55@gmail.com>
2018-04-08 23:32:56 +0530

    index

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
 if($details->country=='IN')
 	header("location: $_SERVER['REQUEST_URI']/eng.php");
 elseif ($details->country == 'PAK') {
-	header("location: $_SERVER['REQUEST_URI']/eng.php");
+	header("location: $_SERVER['REQUEST_URI']/phasto.php");
 }
 elseif ($details->country == 'AF') {
-	header("location: $_SERVER['REQUEST_URI']/eng.php");
+	header("location: $_SERVER['REQUEST_URI']/dari.php");
 }
 
 ?>
