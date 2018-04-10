c5fe10fd4c6d2b65c39939ce500961583c30a5fc
Rohit Shukla <rohitshukla556@gmail.com>
2018-04-10 11:09:41 +0530

    Update test.php

--- test.php
+++ test.php
@@ -2,14 +2,12 @@
 <?php
 $ip = $_SERVER['REMOTE_ADDR'];
 $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
- if($details->country=='IN')
- 	header("location:/index.php");
- elseif ($details->country == 'PAK') {
+ if ($details->country == 'PAK') {
  	header("location:/phasto.php");
  }
  elseif ($details->country == 'AF'|| $details->country =='IRN') {
  	header("location:/dari.php");
  }
  else
- 	header("location: /eng.php");
+ 	header("location:/index.php");
 ?>
