d631f1ef4c3b38033dea079b28c78556ac3c5a4e
rishabh.chandel <rishabh.chandel@ebizontek.com>
2018-05-21 23:22:08 +0530

    url cleaning

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
-<!DOCTYPE html>
+<?php
+$ip = $_SERVER['REMOTE_ADDR'];
+$details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
+ if ($details->country == 'PAK') {
+   header("location:/phasto");
+ }
+ elseif ($details->country == 'AF'|| $details->country =='IRN') {
+   header("location:/dari");
+ }
+
+?>
+
 <html>
    <head>
       <title>Tamaaas - Free Calling & Chat</title>
